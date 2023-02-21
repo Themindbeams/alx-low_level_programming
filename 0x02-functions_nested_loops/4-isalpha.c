666d5053523f86cb38d818050267a7b8186675a1
Ibrahim <iboy060520@gmail.com>
2023-02-21 14:24:05 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - Function that checks for alphabetic character
- *
+ * @c: is the int that will use for the argument of the function
  * Return: Always 0 (Success)
  */
 int _isalpha(int c)
