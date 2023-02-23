1fc7282af30aab6b77d3a28d773056dc9cff85ef
Ibrahim <iboy060520@gmail.com>
2023-02-23 15:02:49 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,39 +2,39 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = 1; i <= 100; i++)
-					{
-								if (i % 3 == 0 && i % 5 != 0)
-											{
-															printf(" Fizz");
-																	}
-										else if (i % 5 == 0 && i % 3 != 0)
-													{
-																	printf(" Buzz");
-																			}
-												else if (i % 3 == 0 && i % 5 == 0)
-															{
-																			printf(" FizzBuzz");
-																					}
-														else if (i == 1)
-																	{
-																					printf("%d", i);
-																							}
-																else
-																			{
-																							printf(" %d", i);
-																									}
-																	}
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
 
-				printf("\n");
+	printf("\n");
 
-					return (0);
+	return (0);
 }
