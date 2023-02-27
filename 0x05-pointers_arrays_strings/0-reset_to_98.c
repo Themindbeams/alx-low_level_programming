db1e42582ade9d98be5ecc0977732c89cd681cd3
Ibrahim <iboy060520@gmail.com>
2023-02-27 11:45:27 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer and update the value to print
+ * @q: pointer
+ * Return: void
+ */
+
+void reset_to_98(int *q)
+{
+	*q = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C programming on pointers, arrays, and strings
